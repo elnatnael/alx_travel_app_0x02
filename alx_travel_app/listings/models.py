2b0dc8b045959b907dfd7be5f6a216ec3d20ea21
elnatnael <meranat9104@gmail.com>
2025-06-05 11:55:29 +0300

    corrected Django Project Setup with API Documentation and Database Configuration

---
+++ alx_travel_app/listings/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+# Create your models here.
